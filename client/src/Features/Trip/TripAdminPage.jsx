e1533b469a3b91a63ddaf652e6312fb5ec9c1bec
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-10-03 09:14:20 -0700

    working on tripadminpage

--- client/src/Features/Trip/TripAdminPage.jsx
+++ client/src/Features/Trip/TripAdminPage.jsx
@@ -10,26 +10,50 @@ export default function TripAdminPage({ trip_id }) {
   function handleTripData(trip_id, user_id) {
     setTripData(!tripData);
   }
-
+// get trip 
   useEffect(() => {
     async function getSingleTrip() {
       const response = await fetchSingleTrip(trip_id);
       console.log("loading trip", response);
       setTripData(response);
     }
+// delete trip member 
+  useEffect(() =>{
+    async function deleteMember() {
+      const response = Await deleteGroupMember(user_id,trip_id); 
+      console.log("deleting trip", response);
+
+    }
+  })
+  function DeleteButton(){
+    const handleDelete = () => { 
+      alert ('Group Member Deleted');
+    };
+  function DeleteTrip() {
+    const handleDelete = () => { 
+      alert ("Trip Deleted");
+    }
+  }
+  }
   });
   return (
     <div>
       <h1> TripAdminPage</h1>
-      <form>
-        {/* <label for="tripname">Trip Name:</label><br>
-        <input type="text" id="tripname" name="tripname" /><br /> 
-        <label for="tripname">Trip Name:</label><br>
-        <input type="text" id="tripname" name="tripname" /><br /> 
-        <label for="addtraveler">Add Traveler</label><br>
-        <input type="email" id="addtraveler" name="add traveler" /><br /> 
-        show all the travelers for the trip based on trip_id */}
+      <h2> tripname</h2>
+      <form> 
+      <label for ="addTraveler">Add New Traveler:</label>
+      <input type="email" id="email" placeholder="Email" /> <br />
+      <button type="submit" value="submit" /> <br /> 
+      {/* display emails of trip members  */}
+       <br /> 
+       <DeleteButton onClick={handleDelete} />
+        <DeleteTrip onClick={handleTripDelete} /> 
       </form>
     </div>
   );
 }
+
+
+// store new travelers 
+// store trip 
+// 
