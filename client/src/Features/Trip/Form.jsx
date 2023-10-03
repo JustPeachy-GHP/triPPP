cfb92d4a8547829759fa108c8752ecb5bdc10db8
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-10-03 09:15:28 -0700

    form stuffs

--- client/src/Features/Trip/Form.jsx
+++ client/src/Features/Trip/Form.jsx
@@ -13,3 +13,17 @@ export default function Form() {
 }
 
 // tripform data + vibecheck data => form.js => helpers => database
+
+// if the user isn't logged in, direct them to the log in page
+
+// if the user is logged in, direct them to our trip form
+
+// with the user_id associted with the user, connect that to the trip you make
+
+if (user_id) {
+  // navigate to tripform
+  useNavigate();
+} else {
+  // navigate to login
+  useNavigate();
+}
