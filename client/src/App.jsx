8da6622925283c2f442d9263fcfb59cfd92b91aa
Amy KWong <amykwong6@gmail.com>
2023-10-09 16:52:14 -0400

    cleaned up comments and organized some code lines around and deleted dups

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,14 @@
-// import { groupmembs, groups, journals, trips, users } from '../../server/db/seedData';
-
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./Features/Display/Navbar";
-import Muidrawer from "./Features/Display/Muidrawer";
 import Navtitle from "./Features/Display/Navtitle";
-// import Navtitle from './Features/Display/Navtitle';
-// import AllJournalEntries from '../Features/Journal';
 import Test from "./Features/Test/Test";
 import DisplayTest from "./Features/Test/DisplayTest";
 import Login from "./Features/Auth/Login";
 import Registration from "./Features/Auth/Registration";
 import UserLanding from "./Features/User/UserLanding";
-// import LocationsInfoWindow from './Features/LocationsComponents/LocationsInfoWindow';
 import ErrorBoundary from "./Features/Display/ErrorBoundary";
 import { GoogleMapsContextProvider } from "./context/googleMapsContext";
 import "./App.css";
-// import MemoizedLocationsMap from './Features/LocationsComponents/LocationsMap';
-// import Form from "./Features/Trip/Form";
-
 import LandingPage from "./Features/Trip/TripForm/LandingPage";
 import TripForm from "./Features/Trip/TripForm/TripForm";
 import Itinerary from "./Features/Itinerary/ItineraryView";
@@ -37,40 +26,25 @@ function App() {
       <ErrorBoundary>
         <GoogleMapsContextProvider>
           <Navtitle />
-          <br />
-          <br />
           <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<LocationsPage />} />
             <Route path="/test" element={<Test />} />
             <Route path="/displaytest" element={<DisplayTest />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Registration />} />
-
             <Route path="/userlanding" element={<UserLanding />} />
             <Route path="/trips" element={<LandingPage />} />
             <Route path="/tripform" element={<TripForm />} />
             <Route path="/tripadminpage" element={<TripAdminPage />} />
             <Route path="/tripadminpage/1" element={<TripAdminPage />} />
-
-            <Route path="/home" element={<LocationsPage />} />
-
-            <Route path="/itinerary" element={<Itinerary />} />
-            <Route
-              path="/journals/:user_id/:trip_id"
-              element={<AllJournalEntries />}
-            />
-            <Route
-              path="/journals/:journal_id"
-              element={<SingleJournalEntry />}
-            />
+            <Route path="/journals" element={<AllJournalEntries />} />
             <Route path="/journalform" element={<CreateJournalForm />} />
-            <Route
-              path="/journals/:journal_id/edit"
-              element={<EditJournalForm />}
-            />
-
-            {/* <Route path="/register" element={<Registration/>}/> */}
-            {/* <Route path="/display" element={<Display/>}/>
-            {/* <Route path="/journals" element={<AllJournalEntries/>}/> */}
+            <Route path="/journals/:user_id/:trip_id" element={<AllJournalEntries />}/>
+            <Route path="/journals/:journal_id" element={<SingleJournalEntry />}/>
+            <Route path="/journals/:journal_id/edit" element={<EditJournalForm />}/>
+            <Route path="/itinerary" element={<ItineraryPage />} />
+            <Route path="/locations" element={<LocationsPage />} />
           </Routes>
         </GoogleMapsContextProvider>
       </ErrorBoundary>
@@ -79,11 +53,3 @@ function App() {
 }
 
 export default App;
-
-//need to import the compenets for all these navagation - need the component files name for the nav to work
-//All those up there is like a placeholder
-
-//moved this here bc using drawertwo
-// <Muidrawer>
-//   <Navbar/>
-// </Muidrawer>
--- client/src/Features/Display/Navbar.jsx
+++ client/src/Features/Display/Navbar.jsx
@@ -1,31 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  console.log("hi");
+  console.log("hi,this is from Navbar");
 
   return (
-
     <div className="navBar" id="navbar">
-      <Link to="/home">Home </Link> <br></br>
-   
       <Link to="/">Home </Link>  <br></br>
-      <Link to="/destination"> Map </Link>  <br></br>
-      <Link to="/locations"> Location </Link> <br></br>
-
-    
-
-      <Link to="/display"> Map </Link> <br></br>
-
-      <Link to="/journal"> Journal </Link> <br></br>
-      <Link to="/itinerary "> Itinerary </Link> <br></br>
-
-      <Link to="/trips"> Trip </Link> <br></br>
-      <Link to="/login"> Login </Link> <br></br>
-      <Link to="/about"> About </Link>
+      <Link to="/tripform"> Make A Trip</Link> <br></br> 
+      <Link to="/userlanding"> My Trips </Link> <br></br>  
+      <Link to="/journals"> Journal </Link> <br></br> 
+      <Link to="/login"> Login </Link> <br></br>  
+      {/* <Link to="/about"> About </Link> */}
     </div>
   );
 }
 
-
-//what is needed to be shown
-//would need a home page, see users?, see group(yourself)
