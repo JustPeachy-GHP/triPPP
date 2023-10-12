e1cc4effff4b1aca8f68f4b14c67a7721c78a150
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-10-12 12:31:08 -0500

    created slices

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit"
 import testReducer from "./slices/testSlice"
 import authReducer from "./slices/authSlice"
+import tripsReducer from "./slices/tripsSlice"
+import mapsReducer from "./slices/mapsSlice"
 
 export default configureStore({
     reducer: {
         test: testReducer,
         auth: authReducer,
+        trips: tripsReducer,
+        maps: mapsReducer,
     }
 })
