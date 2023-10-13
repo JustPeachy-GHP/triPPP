86d16ed6ce7a3639a461093ced77bf85e9a88911
STayengco <137461370+STayengco@users.noreply.github.com>
2023-10-13 06:38:41 -0700

    added redux-persist

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit"
-import testReducer from "./slices/testSlice"
-import authReducer from "./slices/authSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import sessionStorage from "redux-persist/es/storage/session";
+import testReducer from "./slices/testSlice";
+import authReducer from "./slices/authSlice";
+// import tripsReducer from "./slices/tripsSlice"
+// import mapsReducer from "./slices/mapsSlice"
 
-export default configureStore({
-    reducer: {
-        test: testReducer,
-        auth: authReducer,
-    }
-})
+
+const rootReducer = combineReducers({
+  test: testReducer,
+  auth: authReducer,
+//   maps: mapsReducer,
+//   trips: tripsReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: sessionStorage,
+  // blacklist: ['test', 'auth']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
