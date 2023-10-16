08ac7815f59fd7befdd240d92aac3a68b064beda
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-10-16 15:35:20 -0400

    port and helpers

--- client/src/helpers/itinerary.js
+++ client/src/helpers/itinerary.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:8080/api/itineraryitems";
+const baseUrl = "http://localhost:8081/api/itineraryitems";
 
 export async function checkRatings(checkItinObject) {
   try {
