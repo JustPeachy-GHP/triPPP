1687574cfbb17bc816f8accc34d866b7e6ec19c6
STayengco <137461370+STayengco@users.noreply.github.com>
2023-10-17 13:12:48 -0700

    db connection string for deploy

--- server/db/client.js
+++ server/db/client.js
@@ -2,9 +2,9 @@
 const { Client } = require("pg");
 
 //Establishing connect to database (like how we do with http://)
-// const dbName = 'triPPP'
+const dbName = 'trippp'
 
-const client = new Client(`postgres://localhost:5432/trippp`)
+const client = new Client(`postgres://trippp_user:3h7D0Roc9T960qWcLKqcLZL9embhidAR@dpg-cknd5ih1rp3c73b51ahg-a/trippp`)
 
 
 //Export for use in other files
