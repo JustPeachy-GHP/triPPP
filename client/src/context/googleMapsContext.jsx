9a10b384c2ef4a482f66326f1d86a0818151c16f
STayengco <137461370+STayengco@users.noreply.github.com>
2023-10-18 10:10:55 -0700

    changed to process.env key for google map key

--- client/src/context/googleMapsContext.jsx
+++ client/src/context/googleMapsContext.jsx
@@ -1,7 +1,8 @@
 import {createContext, useContext, useEffect, useState } from 'react';
 import { useJsApiLoader } from "@react-google-maps/api";
 
-const API_KEY = import.meta.env.GOOGLE_MAPS_API_KEY;
+// const API_KEY = import.meta.env.GOOGLE_MAPS_API_KEY;
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
 // Define libraries as a constant outside of the component
 const libraries = ["places"];
